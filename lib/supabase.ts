3986b06223664b6dbf2e2334161bd2efd674644e
huonglandoan <lan.doanhuonglan@gmail.com>
2026-05-30 13:23:26 +0700

    update supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+// Dòng này giúp bốc đúng biến ANON_KEY mà bạn vừa sửa trên Vercel
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''; 
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
